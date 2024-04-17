de5f31ebf55d7c727868df271f5741db57e32ef6
ValeriD <valeri@gostartups.net>
2024-04-17 08:49:50 +0300

    Add more information

--- 10g/lesson9/exercise.cpp
+++ 10g/lesson9/exercise.cpp
@@ -7,7 +7,11 @@ using namespace std;
 // The task is to implement the two child classes which should implement
 // the two abstract methods and a Rule of 3 (Constructor, Copy constructor, copy operator=).
 // There is no limitation on fields and methods as long as the functionality is implemented.
+// Add getters and setters
 // Make sure to verify the parameters where it is required using exceptions!
+
+// Link for the area calculation https://mathmonks.com/polygon/area-of-polygon
+// You can add an Enum of the supported polygons and based on the type use different formula.
 //----------------------------------------------------------------
 
 class Figure
